1654c038c7e198f2cc73209d5df4c9fe83d9c9e4
shwetaparmar2807 <shwetaparmar2807@gmail.com>
2025-02-06 11:53:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function firstWord(s) {
   // your code here
+
+ str = str.trim();
+	let spaceIndex = str.indexOf(' ');
+    
+    return spaceIndex === -1 ? str : str.substring(0, spaceIndex);
 }
+
+console.log(firstWord('see and stop'));  
+console.log(firstWord(' Hello World!')); 
+console.log(firstWord('12345'));         
+console.log(firstWord(''));              
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
